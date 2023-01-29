ae10fae0a59959bea437056d90f39fcda1daf560
pritamhate <pritam.hate@outlook.com>
2023-01-29 13:34:17 +0530

    cart funtionality added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,5 @@
 const mCards = document.getElementById('mobileCards');
+const cartList = document.getElementById('cartItem');
 async function prit() {
     console.log("indide the prit");
 
@@ -14,28 +15,24 @@ d.then(data => {
     let products = data.products;
     //console.log(products);
     for (product of products) {
-        console.log(product);
+        //console.log(product);
         mCards.innerHTML += `
-            <div class="col mb-5">
+            <div class="col-12 col-md-4 mb-5">
                 <div class="card h-100">
-                    <!-- Sale badge-->
-                    <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale
+                    <div class="badge bg-danger text-white position-absolute" style="top: 0.5rem; right: 0.5rem">-${Math.round(product.discountPercentage)}%
                     </div>
-                    <!-- Product image-->
                     <img class="card-img-top" src="${product.images[0]}" alt="${product.title}" />
-                    <!-- Product details-->
                     <div class="card-body p-4">
                         <div class="text-center">
-                            <!-- Product name-->
-                            <h5 class="fw-bolder">${product.title}<br><span>${product.brand}</span></h5>
-                            <!-- Product price-->
+                            <h5 class="fw-bolder" id="productTitle">${product.title}</h5>
+                            <span>${product.brand}</span>
+                            <br>
                             <span class="text-muted text-decoration-line-through">$${product.price}</span>
-                            <span>$${product.price - (product.price * product.discountPercentage / 100)}</span>
+                            <b>$${Math.round(product.price - (product.price * product.discountPercentage / 100))}</b>
                         </div>
                     </div>
-                    <!-- Product actions-->
                     <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a>
+                        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="javascript:void(0)" onclick="addtocart(this)" id="addToCart">Add to cart</a>
                         </div>
                     </div>
                 </div>
@@ -43,3 +40,40 @@ d.then(data => {
         `;
     }
 })
+
+let countOfCart = 0;
+let cartCount = document.getElementById('cartCount');
+cartCount.innerText = countOfCart;
+let cartItems = [];
+
+//add items to cart
+function addtocart(e) {
+    let parentNode = e.parentNode.parentNode.parentNode;
+    let childNodes = parentNode.childNodes;
+
+    let productName = childNodes[5].childNodes[1].childNodes[1].innerText;
+    let productPrice = childNodes[5].childNodes[1].childNodes[9].innerText;
+
+    let cartItem = {
+        pname: productName,
+        pprice: Number(productPrice.replace("$","")),
+    };
+
+    cartItems.push(cartItem);
+
+    countOfCart = countOfCart + 1;
+    cartCount.innerText = countOfCart;
+
+    for (item of cartItems) {
+        cartItems = [];
+        console.log(item.pprice);
+
+        cartList.innerHTML+= `
+            <li class="list-group-item">
+                <span>${item.pname}</span>
+                <b>$${item.pprice}</b>
+                <button class="btn btn-danger">Delete</button>
+            </li>
+        `;
+    }
+}
